87c88d2a541fbfd7e8e98fe3a741e86132360f91
CytBlueSky <cyt_2012_82@126.com>
2018-05-28 15:07:32 +0800

    重新提交

---
+++ app/src/main/java/com/example/cn/myapplication/modal/Person.java
@@ -0,0 +1,17 @@
+package com.example.cn.myapplication.modal;
+
+/**
+ * Created by Administrator on 2018-05-28.
+ */
+
+public class Person {
+    public String name;
+    public String phone;
+    public double sel;
+
+    public Person(String name, String phone, double sel) {
+        this.name = name;
+        this.phone = phone;
+        this.sel = sel;
+    }
+}
